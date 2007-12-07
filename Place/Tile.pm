54e3e63a48228f1d796fcfd4a5e3c1ad9ad8d5e9
Stephen Weeks <tene@allalone.org>
2007-12-06 23:57:32 -0700

    A little bit nicer.

--- Place/Tile.pm
+++ Place/Tile.pm
@@ -16,15 +16,15 @@ has up           => (is=>'rw',isa=>'Place::Tile');
 has down         => (is=>'rw',isa=>'Place::Tile');
 
 method BUILD ($params) {
-    ./floor_symbol($params->{'symbol'});
-    ./floor_color($params->{'color'} || undef);
+    $.floor_symbol = $params->{'symbol'};
+    $.floor_color = $params->{'color'} || undef;
 }
 
 method enter ($obj) {
     $obj->tile($self);
 
-    ./symbol($obj->symbol);
-    ./color($obj->color);
+    $.symbol = $obj->symbol;
+    $.color = $obj->color;
     ./add($obj);
     ./draw();
 }
@@ -33,22 +33,22 @@ method leave ($obj) {
     ./remove($obj);
     my @l = ./contents;
     if(@l) {
-        ./symbol($l[-1]->symbol );
-        ./color($l[-1]->color );
+        $.symbol = $l[-1]->symbol;
+        $.color = $l[-1]->color;
     }
     else {
-        ./symbol($.floor_symbol);
-        ./color($.floor_color);
+        $.symbol = $.floor_symbol;
+        $.color = $.floor_color;
     }
     ./draw();
 }
 
 method remove ($obj) {
-    ./contents([grep {! $_ == $obj} ./contents()]);
+    $.contents = [grep {! $_ == $obj} ./contents()];
 }
 
 method add ($obj) {
-    ./contents([./contents(), $obj]);
+    $.contents = [./contents(), $obj];
 }
 
 1;
