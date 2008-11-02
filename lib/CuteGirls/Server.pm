cca00f3e34dc5ed332ea4ccc646841abdc84def6
Stephen Weeks <tene@allalone.org>
2008-11-02 00:04:05 -0600

    Finish the rest of the first draft of comments for Server.pm

--- lib/CuteGirls/Server.pm
+++ lib/CuteGirls/Server.pm
@@ -262,37 +262,80 @@ sub add_player {
     # register a callback for the player's regen tick
     $kernel->delay_set('tick',rand() + scaled_logistic($race->{limbs},20));
 }
+
+=head1 C<object_move_rel>
+
+Move an object relative to its current position
+
+=cut
+
 sub object_move_rel {
     my ($kernel, $session, $heap, $id, $ox, $oy) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1, ARG2];
     my $player = $place->objects->{$id};
     my $dest = $player->get_tile_rel($ox,$oy);
 
+    # don't move it if the hackish "can't hold anything more" flag is set on the destination tile
     return unless $dest->vasru;
+
+    # update the state of the map
     $player->tile->leave($player);
     $dest->enter($player);
     $player->tile($dest);
 
+    # broadcast the event to all clients
     $kernel->post($server_session, 'broadcast', ['object_move_rel', $id, $ox, $oy]);
 }
+
+=head1 C<player_move_rel>
+
+Add a 'move' event to the player's "actions" queue.
+Schedule a handler event if this is the only item in the action queue.
+
+=cut
+
 sub player_move_rel {
     my ($kernel, $session, $heap, $ox, $oy) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1];
     my $self = $place->objects->{$session->ID};
     push @{$self->{actions}}, ['move',$ox,$oy];
+    # If the item we just added is the only one, then there's no handler
+    # event already scheduled
     unless (@{$self->{actions}} > 1) {
         $kernel->delay_set('act',scaled_logistic(13-$self->limbs,5)/4);
     }
 }
+
+=head1 C<tick>
+
+Handle HP regen here.  We also will need to handle shit like poison, some special effects, some racial shit, anything that depends on the character's metabolism.
+
+=cut
+
 sub tick {
     my ($kernel, $session, $heap) = @_[KERNEL, SESSION, HEAP];
+
+    # grab the player for this client session
     my $self = $place->objects->{$session->ID};
-    return unless $self;
+    return unless $self; # bail out if the player is missing (has quit, f.e.)
     if ($self->cur_hp < $self->max_hp) {
+        # some random bullshit algorithm
         my $next = int($self->cur_hp + $self->organs/4 + $self->physical/2 + rand(2));
+        # set cur_hp to min of max_hp and previous cur_hp + bullshit
         $self->cur_hp(($next > $self->max_hp) ? $self->max_hp : $next );
+
+        # send a change event to every client
         $kernel->post($server_session, 'broadcast', ['change_object', $self->id, {'cur_hp'=>$self->cur_hp}]);
     }
+
+    # schedule another tick at now + bullshit
     $kernel->delay_set('tick',rand() + 20/$self->limbs);
 }
+
+=head1 C<act>
+
+Process an item from the actions queue and schedule the next event.
+
+=cut
+
 sub act {
     my ($kernel, $session, $heap) = @_[KERNEL, SESSION, HEAP];
     my $self = $place->objects->{$session->ID};
@@ -301,21 +344,41 @@ sub act {
     if ($act eq 'move') {
         $kernel->call($session, 'object_move_rel', $session->ID, @args);
     }
-    if (@{$self->{actions}} > 0) {
+    if (@{$self->{actions}} > 0) { # there are still items in the queue
+        # schedule another run at now+bullshit
         $kernel->delay_set('act',scaled_logistic(13-$self->limbs,5)/4);
     }
 }
+
+=head1 C<attack>
+
+Try to kill a dude.
+
+=cut
+
 sub attack {
     my ($kernel, $session, $heap, $id, $ox, $oy) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1, ARG2];
+    # grab the appropriate character for the current client
     my $self = $place->objects->{$session->ID};
     my $selfname = $self->username;
     my $other = $place->objects->{$id};
     my $othername = $other->username;
+
+    # find the tile in the direction specified
     my $dest = $self->get_tile_rel($ox,$oy);
+
+    # check to make sure the dude specified is in the tile.  bail out otherwise
     return unless $dest == $other->tile;
+
+    # debugging on the server console
     print $self->symbol, '→', $other->symbol, "\n";
+
+    # We'll deal exactly bullshit damage.
     my $damage = 5 + $self->muscle + int(rand($self->limbs));
+
+    # Check for evasion unless the other dude has a limbs stat 0 o rbelow
     unless ($other->limbs <= 0) {
+        # difference between the limbs stats
         my $diff = $other->limbs - $self->limbs;
         my $evade = scaled_logistic($diff,10);
         if (rand() < $evade) {
@@ -324,54 +387,117 @@ sub attack {
         }
     }
     $other->cur_hp($other->cur_hp - $damage);
+
+    # cancel all pending actions for the dude we just hurt
     delete $other->{actions};
+
+    # announce the attack to everyone
     $kernel->post($server_session, 'broadcast', ['announce', "$selfname hit $othername for $damage damage."]);
     if ($other->alive) {
+        # broadcast the damage to all clients
         $kernel->post($server_session, 'broadcast', ['change_object', $other->id, {'cur_hp'=>$other->cur_hp}]);
     }
     else {
+        # broadcast the death
         $kernel->post($server_session, 'broadcast', ['announce', "$selfname killed $othername."]);
+
+        # death is SO hackish right now.  srsly.
         $kernel->post($server_session, 'broadcast', ['change_object', $other->id, $other->death()]);
     }
 }
+
+=head1 C<drop_item>
+
+Create a bullshit item that will delete itself.
+Eventually this should handle dropping an inventory item.
+
+=cut
+
 sub drop_item {
     my ($kernel, $session, $heap, $symbol,$fg,$bg) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1, ARG2];
+
+    # Everything here should be straightforward.  Ask if you need explained.
     my $player = $place->objects->{$heap->{id}};
     my $obj = Place::Thing->new(fg=>$fg,bg=>$bg,symbol=>$symbol);
     $place->objects->{$obj->id} = $obj;
     $kernel->post($server_session,'broadcast',['drop_item',$heap->{id},$obj->to_hash]);
     $player->tile->enter($obj);
-    my $rand = 5+rand(rand(rand(100)));
+    my $rand = 5+rand(rand(rand(100))); # shitty skewing towards the bottom
     $kernel->delay_set('change_object',$rand/2,$obj->id,{'symbol'=>'°','fg'=>'magenta'});
     $kernel->delay_set('remove_object',$rand,$obj->id);
 }
+
+=head1 C<remove_object>
+
+Delete an object from the map.
+
+=cut
+
 sub remove_object {
     my ($kernel, $session, $heap, $id) = @_[KERNEL, SESSION, HEAP, ARG0];
     $place->objects->{$id}->tile->leave($place->objects->{$id});
     delete $place->objects->{$id};
+
+    # tell everyone about it
     $kernel->post($server_session, 'broadcast', ['remove_object', $id]);
 }
+
+=head1 C<change_object>
+
+Modify an attribute of an object.
+Accepts a hash and for each pair in the hash, calls a method named like the hash key with the value of the hash value.
+
+=cut
+
 sub change_object {
     my ($kernel, $session, $heap, $id, $changes) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1];
     my $obj = $place->objects->{$id};
+
+    # hackish, I think.  Maybe we need some sanity checking on the changes hash
     for my $attr (keys %{$changes}) {
         $obj->$attr($changes->{$attr});
     }
     $kernel->post($server_session, 'broadcast', ['change_object', $id, $changes]);
 }
+
+=head1 C<chat>
+
+Casual conversation.
+
+=cut
+
 sub chat {
     my ($kernel, $session, $heap, $id, $message) = @_[KERNEL, SESSION, HEAP, ARG0, ARG1];
+    # relay the chat message to EVERYONE.  We should be more selective.
     $kernel->post($server_session, 'broadcast', ['chat', $id, $message]);
 }
+
+=head1 C<connection_error>
+
+Clean shit up if there's an error in the client connection.
+
+=cut
 
 sub connection_error {
    my ($kernel, $session, $heap) = @_[KERNEL, SESSION, HEAP];
    return unless defined($place->objects->{$heap->{id}});
+
+   # tell all clients to remove the character object
    $kernel->post($server_session, 'broadcast', ['remove_object', $heap->{id}]);
+
+   # remove all scheduled events
    $kernel->alarm_remove_all();
+
+   # remove the character from the map
    $place->objects->{$heap->{id}}->tile->leave($place->objects->{$heap->{id}});
    delete $place->objects->{$heap->{id}};
 }
+
+=head1 C<connection_broadcast>
+
+Send a message to a connected client.  I don't know why we have this.  It's not used anywhere, and is a trivial wrapper.  Maybe for scheduling...
+
+=cut
 
 sub connection_broadcast {
    my ($kernel, $session, $heap, $message) = @_[KERNEL, SESSION, HEAP, ARG0];
